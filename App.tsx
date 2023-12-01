64c1b86b6fa1388817d4f69afc2a522ffc0788c1
tung_le_thanh <tung_le_thanh@bita.jp>
2023-12-01 16:09:17 +0700

    init resource

---
+++ App.tsx
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View style={styles.cardBoard}>
+          <View>
+            <View>
+              <Text>Lê Thanh Tùng</Text>
+            </View>
+            <View>
+              <Text>Kim Cương</Text>
+            </View>
+          </View>
+          <View>
+            <Text>Đổi 1705 BEAN</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  heading: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#aaa',
+  },
+  cardBoard: {
+    flexDirection: 'row',
+    backgroundColor: '#333',
+    padding: 20,
+  },
+});
+
+export default App;
